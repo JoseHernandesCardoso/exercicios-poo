544e9ded61acb442319ac29489584abd0dc854cc
JoseHernandesCardoso <josehhernandescardoso@gmail.com>
2026-05-13 17:32:41 -0300

    Atividades Feitas

---
+++ Lista 3/funcionario/src/main/java/com/jose/Main.java
@@ -0,0 +1,9 @@
+package com.jose;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario ronaldao = new Gerente("Ronaldo", 3562.41, "2B");
+        
+        ronaldao.exibir();
+    }
+}
