e2547129ab0b11b2cdfd152eede46c630b6fea85
JoseHernandesCardoso <josehhernandescardoso@gmail.com>
2026-05-26 14:00:13 -0300

    Trabalho jogo da velha concluido

--- Jogo da Velha/jogo-da-velha/src/main/java/com/microplasticos/Main.java
+++ Jogo da Velha/jogo-da-velha/src/main/java/com/microplasticos/Main.java
@@ -1,7 +1,11 @@
 package com.microplasticos;
 
+import com.microplasticos.view.JogoDaVelhaView;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JogoDaVelhaView jogo = new JogoDaVelhaView();
+
+        jogo.iniciaPartida();
     }
 }
