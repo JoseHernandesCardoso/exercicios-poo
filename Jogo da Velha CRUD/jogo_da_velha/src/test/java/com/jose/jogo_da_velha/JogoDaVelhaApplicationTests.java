8385aab0b54f2f99af5ebfd7dc31431f64abb486
JoseHernandesCardoso <josehhernandescardoso@gmail.com>
2026-06-30 17:37:13 -0300

    Criação do projeto do Jogo da Velha com CRUD

---
+++ Jogo da Velha CRUD/jogo_da_velha/src/test/java/com/jose/jogo_da_velha/JogoDaVelhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jose.jogo_da_velha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogoDaVelhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
